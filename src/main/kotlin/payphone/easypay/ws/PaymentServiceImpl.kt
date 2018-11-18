d7afc96cf46acb4a8b7ead73af89ffd6cab2059d
Daniel <nieltansah@gmail.com>
2018-11-18 17:41:51 +0700

    ws: 1st try of process engine injection in PaymentServiceImpl

--- src/main/kotlin/payphone/easypay/ws/PaymentServiceImpl.kt
+++ src/main/kotlin/payphone/easypay/ws/PaymentServiceImpl.kt
@@ -1,15 +1,28 @@
 package payphone.easypay.ws
 
+import org.camunda.bpm.engine.RuntimeService
 import java.math.BigDecimal
+import javax.inject.Inject
 import javax.jws.WebService
 
 @WebService(
         serviceName = "PaymentService", name = "Payment", portName = "payment",
         endpointInterface = "payphone.easypay.ws.PaymentService")
 open class PaymentServiceImpl : PaymentService {
+    @Inject
+    lateinit var runtimeService: RuntimeService
+
     override fun getPaymentMethods(): Array<PaymentMethod> = emptyArray()
 
-    override fun beginPayment(paymentMethodId: String, amount: BigDecimal): String = ""
+    override fun beginPayment(paymentMethodId: String, amount: BigDecimal): String {
+        val processVariables = mutableMapOf<String, Any>(
+                "payment-method-id" to paymentMethodId, "amount" to amount)
+
+        runtimeService.startProcessInstanceByMessage(
+                "payment-request", processVariables)
+
+        return ""
+    }
 
     override fun getPaymentDetail(paymentId: String): PaymentDetail = PaymentDetail()
 }
